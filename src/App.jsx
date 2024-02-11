8d34a4d19dd58f5085403617ea9a7bd313a0842b
Rifat Risam <rifatrabby78@gmail.com>
2024-02-11 21:28:17 +0600

    completed navbar and banner with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Shared/Navbar";
+
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
     </>
   );
 }
---
+++ src/Components/Shared/Banner.jsx
@@ -0,0 +1,35 @@
+const Banner = () => {
+  return (
+    <div
+      className="hero md:h-[500px] bg-cover text-black"
+      style={{
+        backgroundImage:
+          "url(https://i.ibb.co/ck7bpMF/16078483-low-poly-banner-design-1711.jpg)",
+      }}
+    >
+      <div className="hero-content flex-col-reverse md:flex-row-reverse ">
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <img
+            src="/banner.png"
+            className=" md:max-w-xs lg:max-w-lg rounded-lg bg-transparent"
+          />
+        </div>
+        <div className="w-full  md:w-1/2 text-center md:text-left">
+          <h1 className="text-2xl lg:text-5xl font-bold ">
+            Manage Your Tasks!
+          </h1>
+          <p className="lg:py-6">
+            Effortlessly organize tasks, track progress, and boost productivity.
+            Our intuitive platform streamlines task management, empowering you
+            to achieve more in less time. Simplify your workflow today!
+          </p>
+          <a className="btn btn-outline text-black hover:bg-blue-300 hover:text-black ">
+            Let{"'"}s Explore
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
